db2c46b677c13df0d9f2c746de5b20014aeb7818
KeshavSaini2405 <sainikeshav740@gmail.com>
2026-01-02 12:23:58 +0000

    Auto-commit

---
+++ q14.c
@@ -0,0 +1,33 @@
+ #include<stdio.h>
+
+int main()
+{
+    int n,i, j, flag, count=0;
+
+    printf("Enter number of element: ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter %d elements: ", n);
+    for(i=0;i<n;i++){
+        scanf("%d", &a[i]);
+    }
+
+    for(i=0;i<n;i++){
+        if(a[i]<2)
+        continue;
+
+    flag = 0;
+    for(j = 2;j <= a[i]/2;j++){
+        if(a[i]%j==0){
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag==0)
+    count++;
+    }
+    printf("\nTotal no. of prime numbers in the array is %d", count);
+    return 0;
+}
